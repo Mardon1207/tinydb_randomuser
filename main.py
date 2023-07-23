d64c964c508e6789ffec8d6cad0957fac7460042
Mardon1207 <mardonsultonov8@gmail.com>
2023-07-23 11:28:51 +0500

    done

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import requests
 from tinydb import TinyDB
-
-db = TinyDB('data.json')
+from api import url
+db = TinyDB('data.json',indent=4)
+for i in range(100):
+    r=requests.get(url)
+    d=r.json()["results"][0]
+    db.insert({"first_name":d["name"]["first"],"last_name":d["name"]["last"],"age":d["registered"]["age"],"phone":d["phone"],"email":d["email"]})
+    print(i)
